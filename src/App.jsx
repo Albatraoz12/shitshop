433221d27024630b2c3357e65bddd1e93bdc8dfb
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-07 00:01:41 +0200

    create product page to display single product

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './compoment/Navbar';
 import './App.css';
 import Home from './pages/Home';
+import Product from './pages/Product';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<Product />} />
       </Routes>
     </BrowserRouter>
   );
