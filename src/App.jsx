4dca84d3b3b9ac832616a17efec0cc6f27507dd2
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-06 22:49:17 +0200

    create navbar with links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './compoment/Navbar';
+import './App.css';
 
 function App() {
   return (
--- src/compoment/Navbar.jsx
+++ src/compoment/Navbar.jsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <nav>
-      <div>LOGO</div>
-      <div>CART</div>
+    <nav className='navbar'>
+      <div className='logo'>
+        <Link to='/'>
+          <img src='/logo.svg' alt='logo' height={40} width={50} />
+        </Link>
+      </div>
+      <div className='cart'>
+        <Link to='/cart'>
+          <img src='/cart.svg' alt='cart' height={40} width={50} />
+        </Link>
+      </div>
     </nav>
   );
 };
