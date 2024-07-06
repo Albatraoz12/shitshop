1781f10e602936fa740a853123786f36b570141a
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-07 01:20:55 +0200

    add pagination and styling

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
 import Card from './Card';
 
 /* eslint-disable react/prop-types */
 const CardList = ({ products }) => {
-  console.log(products);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(products.length / itemsPerPage);
+
+  // Calculate the products to be displayed on the current page
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, products.length);
+
+  const currentProducts = products.slice(startIndex, endIndex);
+
+  // Handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <div className='products-list-container'>
-      {products &&
-        products.length > 0 &&
-        products.map((product) => <Card key={product.id} product={product} />)}
+    <div>
+      <div className='products-list-container'>
+        {currentProducts.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
+      <div className='pagination'>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (page) => (
+            <button
+              key={page}
+              onClick={() => handlePageChange(page)}
+              disabled={currentPage === page}
+            >
+              {page}
+            </button>
+          )
+        )}
+      </div>
     </div>
   );
 };
