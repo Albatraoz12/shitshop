f58511776892aadf323fcf6cc73127bc080559fa
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-08 19:24:59 +0200

    display cart and add some styling

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,13 +2,30 @@ import { useContext } from 'react';
 import { CartContext } from '../context/CartProvider';
 
 const Cart = () => {
-  const { calculateTotal } = useContext(CartContext);
-
+  const { cart, calculateTotal } = useContext(CartContext);
+  console.log(cart);
   return (
-    <div>
+    <section>
       <h1>Cart</h1>
+      {cart && cart.length > 0 ? (
+        cart.map((item) => (
+          <div key={item.id} className='items-container'>
+            <div className='cart-image'>
+              <figure>
+                <img src={item.image} alt={item.title} />
+              </figure>
+            </div>
+            <div className='cart-title'>
+              <h2>{item.title}</h2>
+              <p>Quantity: {item.quantity}</p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <h2>No items in your cart!</h2>
+      )}
       <h2>Total: {calculateTotal()} €</h2>
-    </div>
+    </section>
   );
 };
 
