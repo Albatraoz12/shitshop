ffe2e1bb7f4a76e6fdf61bd162fe8e025adc048e
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-08 20:19:15 +0200

    display how many items there is in cart if it is greater than 0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../context/CartProvider';
 
 const Navbar = () => {
+  const { cart } = useContext(CartContext);
+
+  // Calculate total number of items in cart
+  const cartItemCount = cart.length;
+
   return (
     <nav className='navbar'>
       <div className='logo'>
@@ -12,6 +19,9 @@ const Navbar = () => {
       <div className='cart'>
         <Link to='/cart'>
           <img src='/cart.svg' alt='cart' height={40} width={50} />
+          {cartItemCount > 0 && (
+            <span className='cart-count'>{cartItemCount}</span>
+          )}
         </Link>
       </div>
     </nav>
