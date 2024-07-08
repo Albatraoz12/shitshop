179ca9591e18d1a6426c76d5523ac1427948ebcd
Albatraoz12 <dimosthenis.emmanouil@chasacademy.se>
2024-07-08 17:35:20 +0200

    create cart route and element

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './pages/Home';
 import Product from './pages/Product';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/product/:id' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
